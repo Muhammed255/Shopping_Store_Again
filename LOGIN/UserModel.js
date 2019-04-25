182de06edad9e730cb9fb57748e0695c86c81bf4
Mahmud Ali <mahmoud.aly.fcih@Gmail.com>
2019-04-25 03:09:49 +0200

    Update UserModel.js
    this user model (class)

--- LOGIN/UserModel.js
+++ LOGIN/UserModel.js
@@ -1,41 +1,23 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/users.model');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
-const userController = {};
+const { Schema } = mongoose;
 
-/**
- * Login Logic
- */
-userController.login = async (req, res, next) => {
-    // Username and password in request
-    const { email, password } = req.body;
-    try {
-        //Check username and password and return ok
-        const user = await User.findOne({ email });
-        if (!user) {
-            const err = new Error(`The email ${email} was not found on our system`);
-            err.status = 401;
-            next(err);
+const UserSchema = Schema({
+    name: { type: String },
+    email: { type: String, required: true, index: true, unique: true },
+    password: { type: String, required: true },
+    joined: { type: Date, default: new Date() }
+});
+
+UserSchema.methods.isPasswordMatch = function (password, hashed, callback) {
+    bcrypt.compare(password, hashed, (err, success) => {
+        if (err) {
+            return callback(err);
         }
+        callback(null, success);
+    });
+};
 
-        user.isPasswordMatch(password, user.password, (err, matched) => {
-            if (matched) {
-                //JWT Secret
-                const secret = process.env.JWT_SECRET;
-                //JWT Expiration
-                const expire = process.env.JWT_EXPIRATION;
-                //Send The Token
-                const token = jwt.sign({ _id: user._id }, secret, { expiresIn: expire });
-                return res.send({ token });
-            }
-
-            res.status(401).send({ error: "Invalid Email/Password" });
-        })
-    } catch (e) {
-        next(e);
-    }
-
-
-}
-
-module.exports = userController;
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
